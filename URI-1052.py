e641c895bbe653807ba797b065a5e103a582dab4
MasumTech <masumrezadiu@gmail.com>
2019-09-04 13:32:13 +0600

    Solution of URI Problem No 1052 in Python

---
+++ URI-1052.py
@@ -0,0 +1,28 @@
+a = input()
+
+
+if int(a)==int(1):
+    print('January')
+elif int(a)==int(2):
+    print('February')
+elif int(a)==int(3):
+    print('March')
+elif int(a) == int(4):
+    print('April')
+elif int(a) == int(5):
+    print('May')
+elif int(a) == int(6):
+    print('June')
+elif int(a) == int(7):
+    print('July')
+elif int(a) == int(8):
+    print('August')
+elif int(a) == int(9):
+    print('September')
+elif int(a)==int(10):
+    print('October')
+elif int(a)==int(11):
+    print('November')
+elif int(a) == int(12):
+    print('December')
+
