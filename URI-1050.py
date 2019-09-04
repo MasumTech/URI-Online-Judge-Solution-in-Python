597ad671386f13f2bb0b84d3e9aa6dd7ffa32b88
MasumTech <masumrezadiu@gmail.com>
2019-09-04 08:47:33 +0600

    Solution of URI Problem No 1050 in Python

---
+++ URI-1050.py
@@ -0,0 +1,20 @@
+a = input()
+
+if int(a)==int(61):
+    print('Brasilia')
+elif int(a)==int(71):
+    print('Salvador')
+elif int(a)==int(11):
+    print('Sao Paulo')
+elif int(a)==int(21):
+    print('Rio de Janeiro')
+elif int(a)==int(32):
+    print('Juiz de Fora')
+elif int(a)==int(19):
+    print('Campinas')
+elif int(a)==int(27):
+    print('Vitoria')
+elif int(a)==int(31):
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
